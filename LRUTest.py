d7b62a9f22541651977e7675026e85378c166127
saragadamraviteja <saragadamraviteja@gmai.com>
2020-04-13 23:34:53 +0530

    commit

---
+++ LRUTest.py
@@ -0,0 +1,27 @@
+from lru_problem import LRUcache
+
+class main:
+    def __init__(self):
+        self.cach = LRUcache(3)
+        self.testcases()
+    
+    def testcases(self):
+        self.cach.put(9)
+        print(self.check([9]))
+        self.cach.put(8)
+        print(self.check([9,8]))
+        self.cach.get(8)
+        print(self.check([8,9]))
+        self.cach.put(7)
+        print(self.check([8,9,7]))
+
+    def check(self, expected):
+        result = self.cach.get_cache()
+        if (len(result) != len(expected)):
+            return False
+        for i,j in enumerate(expected):
+            if(j != result[i]):
+                return False
+            return True
+if __name__ =="__main__":
+    main()
---
+++ lru_problem.py
@@ -0,0 +1,22 @@
+class LRUcache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = []
+
+    def put(self,additem):
+        if len(self.cache) >= self.capacity:
+            self.cache.pop(0)
+        self.cache.append(additem)
+
+    def get(self,getitem):
+        popped = self.cache.pop(self.cache.index(getitem))
+        self.cache.append(popped)
+        return popped
+
+    def get_cache(self):
+        return self.cache
+
+
+if __name__ == "__main__":
+    LRUcache(input("enter size"))
+
