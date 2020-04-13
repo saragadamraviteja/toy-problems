59b8a5e9b2a3e47149f22dde69bbd3c2969a57c8
saragadamraviteja <saragadamraviteja@gmai.com>
2020-04-13 22:54:45 +0530

    i'll let you decide

--- lru.py
+++ lru.py
@@ -4,7 +4,17 @@ class LRUcache:
         self.count = 0
         self.cache = {}
         self.lru ={}
+    def put(self, key,value):
+        pass
+    
+
+    def get(self, key):
+        pass
+
+    def get_cache(self):
+        pass
+
 
 if __name__ == "__main__":
-    objct = LRUcache()
+    objct = LRUcache(4)
 
